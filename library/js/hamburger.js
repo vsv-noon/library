f312ca9ed0249423d8767de9cc37dee51a0e3076
ViacheslavAnisimov <forvsn@gmail.com>
2023-08-04 07:53:33 +0700

    fix: edit hamburger menu behavior

--- library/js/hamburger.js
+++ library/js/hamburger.js
@@ -5,3 +5,10 @@ menuToggle.onclick = function () {
   navigation.classList.toggle("active");
   navigationBtn.classList.toggle("active");
 };
+
+document.addEventListener("click", (el) => {
+  if (!el.target.classList.contains("menu-toggle")) {
+    navigation.classList.remove("active");
+    navigationBtn.classList.remove("active");
+  }
+})
